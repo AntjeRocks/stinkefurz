4888fcb18a0fe67d047bb2840438cf83fcb6a848
Antje Rocks <example@example.com>
2024-09-03 20:50:03 +0200

    remove run_blender ps1 script

--- src/run_blender.ps1
+++
@@ -1,5 +0,0 @@
-$env:PYTHONPATH = {absolute path to src folder}
-
-Start-Process -FilePath {absolute path to blender.exe (blender 4.0.2)} `
-              -ArgumentList "--background", "--python", {absolute path to main.py (in src)} `
-              -NoNewWindow -Wait
